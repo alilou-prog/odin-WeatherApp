28b7d348d75d56023ef3c17dee93b0ee45790a5a
aliou-prog <alimokhati2023@gmail.com>
2025-06-27 15:34:27 +0100

    Get the data of the current day

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const base_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/r
 const api_key = 'TKL67EH2P82E4X4SGW464PEFZ';
 
 const catch_error = (fn) => {
-    return function(...params) {
+    return function (...params) {
         return fn(...params).catch(err => console.error(err))
     }
 }
@@ -14,7 +14,11 @@ const fetch_s = catch_error(fetch)
 async function get_data(city) {
     const response = await fetch_s(`${base_url}/${city}?key=${api_key}`);
     const data = await response.json();
-    console.log(data);
+    return data;
 }
 
-get_data("london");
+function get_temp(data, day) {
+    return data.days[day].temp;
+}
+
+console.log(get_temp(await get_data("london"), 0))
