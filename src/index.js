00c3358e05c66e31d00874b2af7dd27c18b98129
aliou-prog <alimokhati2023@gmail.com>
2025-06-27 14:59:33 +0100

    api_key working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css"
+
+const base_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline'
+const api_key = 'TKL67EH2P82E4X4SGW464PEFZ';
+
+const catch_error = (fn) => {
+    return function(...params) {
+        return fn(...params).catch(err => console.error(err))
+    }
+}
+
+const fetch_s = catch_error(fetch)
+
+async function get_data(city) {
+    const response = await fetch_s(`${base_url}/${city}?key=${api_key}`);
+    const data = await response.json();
+    console.log(data);
+}
+
+get_data("london");
